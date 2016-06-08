58d9dbe0e01dc8e82daf0702fc5aa506e162c8bb
cdjeassittarame <c.djeassittarame@live.fr>
2016-06-08 16:51:59 +0200

    commit2 08062016

--- app/src/main/java/com/example/kabtel/myapplicationcommande/Commande.java
+++ app/src/main/java/com/example/kabtel/myapplicationcommande/Commande.java
@@ -1,18 +1,34 @@
 package com.example.kabtel.myapplicationcommande;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 /**
  * Created by kabtel on 02/06/16.
  */
 public class Commande extends AppCompatActivity {
+
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.commande);
 
+        button = (Button) findViewById(R.id.quitter);
+
+    }
+
+    public void Quitter(View view) {
+        this.finish();
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 
 }
