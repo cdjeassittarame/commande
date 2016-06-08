9213c7adf5a4cd7e9d6ef648d1516e1dcc7c7234
cdjeassittarame <c.djeassittarame@live.fr>
2016-06-08 18:00:41 +0200

    modif final

--- app/src/main/java/com/example/kabtel/myapplicationcommande/MainActivity.java
+++ app/src/main/java/com/example/kabtel/myapplicationcommande/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends Activity {
 
     Button button;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
