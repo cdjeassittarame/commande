e91b263e99e5a287e2d91a0c6273993b3dd4f80a
cdjeassittarame <c.djeassittarame@live.fr>
2016-06-08 16:37:56 +0200

    first commit

---
+++ app/src/main/java/com/example/kabtel/myapplicationcommande/Commande.java
@@ -0,0 +1,18 @@
+package com.example.kabtel.myapplicationcommande;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+/**
+ * Created by kabtel on 02/06/16.
+ */
+public class Commande extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.commande);
+
+    }
+
+}
